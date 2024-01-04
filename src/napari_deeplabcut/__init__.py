9c85c783ed969af3dd986e68cfd7676dad75d296
Kyle Daruwalla <daruwalla@wisc.edu>
2024-01-04 11:18:39 -0500

    Add version to __init__

--- src/napari_deeplabcut/__init__.py
+++ src/napari_deeplabcut/__init__.py
@@ -6,7 +6,7 @@ warnings.filterwarnings("ignore", category=FutureWarning)
 try:
     from ._version import version as __version__
 except ImportError:  # pragma: no cover
-    __version__ = "unknown"
+    __version__ = "0.2.2"
 
 
 from ._reader import (
